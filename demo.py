f8f6927c46173238e49f985c83a385cb6bc9f755
pafable <philafable@gmail.com>
2018-12-05 22:25:27 -0500

    created main.py

--- demo.py
+++ demo.py
@@ -40,5 +40,5 @@ def file_to_set(file_name):
 
 def set_to_file(links,file_name):
     with open(file_name,"w") as f:
-        for 1 in sorted(links):
-            f.write(1+'\n')
+        for l in sorted(links):
+            f.write(l+'\n')
