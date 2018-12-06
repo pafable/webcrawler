aa0db1705319f5940113a70596656cc1ad9c5517
pafable <philafable@gmail.com>
2018-12-05 21:00:02 -0500

    created dir func

--- demo.py
+++ demo.py
@@ -1,6 +1,15 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
+# from urllib.request import urlopen
+# from bs4 import BeautifulSoup
+#
+# html = urlopen("http://pafable.com")
+# bsobject = BeautifulSoup(html.read(),"html.parser")
+# print(bsobject.h1)
 
-html = urlopen("http://pafable.com")
-bsobject = BeautifulSoup(html.read(),"html.parser")
-print(bsobject.h1)
+import os
+
+def create_project_dir(dir):
+    if not os.path.exists(dir):
+        print('Creating the directory ' + dir)
+        os.makedirs(dir)
+
+create_project_dir('thesite')
